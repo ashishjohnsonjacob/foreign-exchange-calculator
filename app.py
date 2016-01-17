76c3de10b952209d5ebed2e3f9ca5b09ced9575b
ashishjacob07 <ashishjohnson.jacob@my.jcu.edu.au>
2016-01-18 00:17:01 +0800

    Updated gui.kv and app.py.

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ class Foreign_Exchange_Calculator(App):
         self.get_full_country_list()
         self.today_date = datetime.datetime.now()
         self.current_date = self.today_date.strftime('%Y/%m/%d')
+        self.current_time = self.today_date.strftime('%H:%M:%S')
 
     def build(self):
         self.title = "Foreign Exchange Calculator"
@@ -35,11 +36,18 @@ class Foreign_Exchange_Calculator(App):
         return self.root
 
     def get_home_country(self):
-        with open('config.txt', encoding='utf8') as list_countries:
-            for line in list_countries:
-                if ',' not in line:
-                    self.home_country = line.strip()
-        self.home_country_details = currency.get_details(self.home_country)
+        try:
+            with open('config.txt', encoding='utf8') as list_countries:
+                for line in list_countries:
+                    if ',' not in line:
+                        self.home_country = line.strip()
+            self.home_country_details = currency.get_details(self.home_country)
+            self.status_bar_text = str("TRIP DETAILS ACCEPTED")
+        except:
+            if  os.path.isfile(confix.txt)
+                self.status_bar_text = str("FILE NOT FOUND")
+            else:
+                self.status_bar_text = str("TRIP DETAILS INVALID")
 
     def set_selected_country(self):
         self.selected_country_details = currency.get_details(self.root.ids.spinner_selection.text)
@@ -61,21 +69,26 @@ class Foreign_Exchange_Calculator(App):
         return self.country_details
 
     def convert(self):
-        self.root.ids.input_selected.text = str(currency.convert(1, self.home_country_details[1],self.selected_country_details[1]))
-        self.root.ids.input_home.text = str(1)
+            self.root.ids.input_selected.text = str(currency.convert(1, self.home_country_details[1],self.selected_country_details[1]))
+            self.root.ids.input_home.text = str(1)
+            self.root.ids.status_bar.text = str("{} ({}) to {} ({})".format(self.home_country_details[1], self.home_country_details[2], self.selected_country_details[1], self.selected_country_details[2]))
 
     def convert_selected_to_home(self):
         try:
             self.root.ids.input_home.text = str(currency.convert(self.root.ids.input_selected.text, self.selected_country_details[1], self.home_country_details[1]))
+            self.root.ids.status_bar.text = str("{} ({}) to {} ({})".format(self.selected_country_details[1], self.selected_country_details[2], self.home_country_details[1], self.home_country_details[2]))
         except:
-            print("ERROR")
+            print("ERROR IN CONVERSION. PLEASE ENTER A VALID AMOUNT")
 
     def convert_home_to_selected(self):
         try:
             self.root.ids.input_selected.text = str(currency.convert(self.root.ids.input_home.text, self.home_country_details[1],self.selected_country_details[1]))
+            self.root.ids.status_bar.text = str("{} ({}) to {} ({})".format(self.home_country_details[1], self.home_country_details[2], self.selected_country_details[1], self.selected_country_details[2]))
         except:
             self.get_current_country_details()
+            self.root.ids.spinner_selection.text = self.current_country
             self.root.ids.input_selected.text = str(currency.convert(self.root.ids.input_home.text, self.home_country_details[1], self.current_country_details[1]))
+            self.root.ids.status_bar.text = str("{} ({}) to {} ({})".format(self.home_country_details[1], self.home_country_details[2], self.selected_country_details[1], self.selected_country_details[2]))
 
     def current_location(self, str_date):
         datetime.datetime.strptime(str_date, '%Y/%m/%d')
@@ -86,6 +99,16 @@ class Foreign_Exchange_Calculator(App):
         for x in date_list[1:]:
             if x[2] > str_date > x[1]:
                 return x[0]
+
+    def updated(self):
+        if not self.root.ids.spinner_selection.text:
+            self.root.ids.spinner_selection.text = str(self.current_country)
+        else:
+            self.convert_home_to_selected()
+        self.root.ids.status_bar.text = str("{}{}".format("UPDATED AT ", self.current_time ))
+
+    def status_bar_clear(self):
+        self.status_bar_text = str(" ")
 
 if __name__ == '__main__':
     Foreign_Exchange_Calculator().run()
